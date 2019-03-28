baf7d8a21f0512abf01ae9d8aa7c143dd9e89550
JuniorImran <47261469+JuniorImran@users.noreply.github.com>
2019-03-28 18:07:40 +0000

    Add files via upload

---
+++ Projet/script.js
@@ -0,0 +1,352 @@
+
+
+
+	/* (AF1) A=Action  F=fruit 1=1er elemennt*/
+    	var AF1='banane';
+  		var AF2='Pasteque';
+   		var AF3='Pomme';
+    	var AF4='Mangue';
+   		var AF5='Fraise';
+        var AF6='Corosol';
+  		var AF7='Citron';
+    	var AF8='Raisin';
+
+        var question= "combien de Kg vous voulez?"
+        var nbre=[''];
+
+		function passageF1(){
+            
+            }
+
+    	function ActionF1(){
+
+           var nbreAF1= parseInt(prompt('Combien de Kg de '+AF1+' vous voulez ?'));
+
+      /*      var nbre=Number(document.getElementsByTagId('nbre').value); */
+ 		
+    	}
+
+        function passageF2(){
+
+                    }
+         function ActionF2(){
+
+            var nbreAF2= parseInt(prompt('Combien de Kg de '+AF2+' vous voulez ?'));
+                        
+        }
+
+        function passageF3(){
+
+                    }
+        function ActionF3(){
+
+            var nbreAF3= parseInt(prompt('Combien de Kg de '+AF3+' vous voulez ?'));
+                        
+        }
+       function passageF4(){
+
+                    }
+         function ActionF4(){
+                        
+            var nbreAF4= parseInt(prompt('Combien de Kg de '+AF4+' vous voulez ?'));
+        }
+           function passageF5(){
+
+                    }
+       function ActionF5(){
+
+        var nbreAF5= parseInt(prompt('Combien de Kg de '+AF5+' vous voulez ?'));
+                        
+        }
+         function passageF6(){
+
+                    }
+         function ActionF6(){
+
+        var nbreAF6= parseInt(prompt('Combien de Kg de '+AF6+' vous voulez ?'));
+                        
+          }
+        function passageF7(){
+
+                    }
+           function ActionF7(){
+
+        var nbreAF7= parseInt(prompt('Combien de Kg de '+AF7+' vous voulez ?'));
+                        
+          }
+        function passageF8(){
+
+
+                    }
+           function ActionF8(){
+
+        var nbreAF8= parseInt(prompt('Combien de Kg de '+AF8+' vous voulez ?'));
+
+         }
+
+         /*Fin fruits*/
+
+         /*Legumes var*/
+
+         var AL1='Ail-blancs';
+         var AL2='Avocat';
+         var AL3='Salade';
+         var AL4='Pommes-de-terre';
+         var AL5='Oignon';
+         var AL6='Concombre';
+         var AL7='Carotte';
+         var AL8='Tomate';
+
+        function passageL7(){
+
+                }
+       function ActionL7(){
+
+        var nbreAL1= parseInt(prompt('Combien de Kg de '+AL1+' vous voulez ?'));
+                        
+            }
+        function passageL2(){
+
+                    }
+          function ActionL2(){
+
+            var nbreAL2= parseInt(prompt('Combien de Kg de '+AL2+' vous voulez ?'));
+                        
+          }
+          function passageL3(){
+
+                    }
+         function ActionL3(){
+
+            var nbreAL3= parseInt(prompt('Combien de Kg de '+AL3+' vous voulez ?'));
+                        
+          }
+           function passageL4(){
+
+                    }
+        function ActionL4(){
+
+            var nbreAL4= parseInt(prompt('Combien de Kg de '+AL4+' vous voulez ?'));
+                        
+                    }
+        function passageL5(){
+
+                    }
+         function ActionL5(){
+
+            var nbreAL5= parseInt(prompt('Combien de Kg de '+AL5+' vous voulez ?'));
+                        
+                    }
+        function passageL6(){
+
+                    }
+        function ActionL6(){
+
+            var nbreAL6= parseInt(prompt('Combien de Kg de '+AL6+' vous voulez ?'));
+                        
+        }
+         function passageL7(){
+
+                    }
+         function ActionL7(){
+
+            var nbreAL7= parseInt(prompt('Combien de Kg de '+AL7+' vous voulez ?'));
+                        
+           }
+        function passageL8(){
+
+                    }
+         function ActionL8(){
+                        
+            var nbreAL8= parseInt(prompt('Combien de Kg de '+AL8+' vous voulez ?'));
+                        
+          }
+
+        /*Fonctions Sucreries*/
+
+        function passageS1(){
+
+                    }
+         function ActionS1(){
+                        
+          }
+       function passageS2(){
+
+                    }
+          function ActionS2(){
+                        
+           }
+          function passageS3(){
+
+                    }
+       function ActionS3(){
+                        
+               }
+        function passageS4(){
+
+                    }
+         function ActionS4(){
+                        
+        }
+        function passageS5(){
+
+                    }
+         function ActionS5(){
+                        
+       }
+        function passageS6(){
+
+                    }
+      function ActionS6(){
+                        
+                    }
+       function passageS7(){
+
+                    }
+         function ActionS7(){
+                        
+                }
+        function passageS8(){
+
+                    }
+        function ActionS8(){
+                        
+                    }
+        /*Alcool*/
+
+        function passageA1(){
+
+                    }
+        function ActionA1(){
+
+         }
+        function passageA2(){
+
+                    }
+        function ActionA2(){
+
+                        
+                    }    
+        function passageA3(){
+
+                    }
+         function ActionA3(){
+                        
+                 }
+        function passageA4(){
+
+                    }
+               function ActionA4(){
+                        
+           }
+        function passageA5(){
+
+                    }
+         function ActionA5(){
+                        
+                    }
+        function passageA6(){
+
+                    }
+         function ActionA6(){
+                        
+                 }
+         function passageA4(){
+
+                    }
+              function ActionA7(){
+                        
+           }
+        function passageA8(){
+
+                    }
+         function ActionA8(){
+                        
+                 }
+
+        /*Plat*/
+
+        function passageP1(){
+
+                    }
+        function ActionP1(){
+
+         }
+        function passageP2(){
+
+                    }
+        function ActionP2(){
+
+                        
+                    }    
+        function passageP3(){
+
+                    }
+         function ActionP3(){
+                        
+                 }
+        function passageP4(){
+
+                    }
+               function ActionP4(){
+                        
+           }
+        function passageP5(){
+
+                    }
+         function ActionP5(){
+                        
+                    }
+        function passageP6(){
+
+                    }
+         function ActionP6(){
+                        
+                 }
+         function passageP4(){
+
+                    }
+              function ActionP7(){
+                        
+           }
+        function passageP8(){
+
+                    }
+         function ActionP8(){
+                        
+                 }
+
+
+        /*Action finale*/
+        function terminé(){
+           
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
